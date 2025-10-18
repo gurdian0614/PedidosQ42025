4a164949ba393abb9f359106a1cdadf793eb041d
gurdian0614 <ivan.gurdian@outlook.com>
2025-10-17 19:22:11 -0600

    Adding data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Creamos el objeto
+Producto producto = new Producto();
+Producto producto1 = new Producto();
+
+producto.Id = 1;
+producto.Nombre = "Laptop";
+producto.Precio = 13500;
+producto.Stock = 10;
+producto.MostrarInformacion();
+producto.DisminuirStock(15);
+producto.MostrarInformacion();
+
+producto1.Id = 2;
+producto1.Nombre = "Mouse";
+producto1.Precio = 250;
+producto1.Stock = 100;
+producto1.MostrarInformacion();
+producto1.DisminuirStock(13);
+producto1.MostrarInformacion();
