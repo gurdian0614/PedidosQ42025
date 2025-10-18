d91a0634e7ad87c61aa5c0f14248c3cd3016c6cc
gurdian0614 <ivan.gurdian@outlook.com>
2025-10-17 19:22:03 -0600

    Create Product Class

---
+++ clases/Producto.cs
@@ -0,0 +1,37 @@
+public class Producto
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public int Stock { get; set; }
+
+    /// <summary>
+    /// Muestra la informacion del producto
+    /// </summary>
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("********** DATOS DEL PRODUCTO **********");
+        Console.WriteLine($"ID: {Id}");
+        Console.WriteLine($"Nombre: {Nombre}");
+        Console.WriteLine($"Precio: {Precio}");
+        Console.WriteLine($"Cantidad disponible: {Stock}");
+        Console.WriteLine();
+    }
+
+    /// <summary>
+    /// Disminuye el inventario
+    /// </summary>
+    /// <param name="Cantidad">Cantidad a disminuir en el inventario</param>
+    public void DisminuirStock(int Cantidad)
+    {
+        if (Stock >= Cantidad)
+        {
+            Stock -= Cantidad;
+        }
+        else
+        {
+            Console.WriteLine($"No hay suficiente stock de {Nombre}");
+            Console.WriteLine();
+        }
+    }
+}
