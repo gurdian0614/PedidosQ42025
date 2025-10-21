c1e0ff9d24c8e548cd6596d5ba7f38ad8c2d13db
gurdian0614 <ivan.gurdian@outlook.com>
2025-10-20 18:28:28 -0600

    Create PEdido class

---
+++ clases/ItemPedido.cs
@@ -0,0 +1,13 @@
+
+public class ItemPedido
+{
+    public Producto Producto { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Subtotal => Producto.Precio * Cantidad;
+
+    public ItemPedido(Producto Producto, int Cantidad)
+    {
+        this.Producto = Producto;
+        this.Cantidad = Cantidad;
+    }
+}
