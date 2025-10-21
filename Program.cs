126bfc012a55c13daee27a05632f9930575a7c52
gurdian0614 <ivan.gurdian@outlook.com>
2025-10-20 19:14:45 -0600

    Update data

--- Program.cs
+++ Program.cs
@@ -6,9 +6,7 @@ Producto producto1 = new Producto();
 producto.Id = 1;
 producto.Nombre = "Laptop";
 producto.Precio = 13500;
-producto.Stock = 10;
-producto.MostrarInformacion();
-producto.DisminuirStock(15);
+producto.Stock = 1500;
 producto.MostrarInformacion();
 
 producto1.Id = 2;
@@ -16,5 +14,12 @@ producto1.Nombre = "Mouse";
 producto1.Precio = 250;
 producto1.Stock = 100;
 producto1.MostrarInformacion();
-producto1.DisminuirStock(13);
+
+Pedido pedido = new Pedido(1);
+pedido.AgregarItem(producto, 150);
+pedido.AgregarItem(producto1, 45);
+pedido.MostrarDetalles();
+
+Console.WriteLine("********** Actualización de Productos **********");
+producto.MostrarInformacion();
 producto1.MostrarInformacion();
