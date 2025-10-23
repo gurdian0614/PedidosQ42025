4e4baca29c95f6841fddc5aa7c68f383058de785
gurdian0614 <ivan.gurdian@outlook.com>
2025-10-22 19:17:29 -0600

    Adding data

--- Program.cs
+++ Program.cs
@@ -23,3 +23,22 @@ pedido.MostrarDetalles();
 Console.WriteLine("********** Actualización de Productos **********");
 producto.MostrarInformacion();
 producto1.MostrarInformacion();
+
+Electronico electronico = new Electronico();
+electronico.Id = 3;
+electronico.Nombre = "iPhone 17 Pro Max";
+electronico.Precio = 52000;
+electronico.Stock = 10;
+electronico.GarantiaMeses = 12;
+electronico.Voltaje = "110V";
+electronico.MostrarInformacion();
+
+Libro libro = new Libro();
+libro.Id = 4;
+libro.Nombre = "El Principito";
+libro.Precio = 250;
+libro.Stock = 1500;
+libro.ISBN = "8536492365322";
+libro.Autor = "Antoine de Saint-Exupéry";
+libro.NumeroPaginas = 170;
+libro.MostrarInformacion();
