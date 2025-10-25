0445d95ef7104bd90fcfc7f7510cb30395e3bae7
gurdian0614 <ivan.gurdian@outlook.com>
2025-10-24 19:17:06 -0600

    Refactor data

--- Program.cs
+++ Program.cs
@@ -1,29 +1,5 @@
 ﻿
 // Creamos el objeto
-Producto producto = new Producto();
-Producto producto1 = new Producto();
-
-producto.Id = 1;
-producto.Nombre = "Laptop";
-producto.Precio = 13500;
-producto.Stock = 1500;
-producto.MostrarInformacion();
-
-producto1.Id = 2;
-producto1.Nombre = "Mouse";
-producto1.Precio = 250;
-producto1.Stock = 100;
-producto1.MostrarInformacion();
-
-Pedido pedido = new Pedido(1);
-pedido.AgregarItem(producto, 150);
-pedido.AgregarItem(producto1, 45);
-pedido.MostrarDetalles();
-
-Console.WriteLine("********** Actualización de Productos **********");
-producto.MostrarInformacion();
-producto1.MostrarInformacion();
-
 Electronico electronico = new Electronico();
 electronico.Id = 3;
 electronico.Nombre = "iPhone 17 Pro Max";
@@ -31,6 +7,12 @@ electronico.Precio = 52000;
 electronico.Stock = 10;
 electronico.GarantiaMeses = 12;
 electronico.Voltaje = "110V";
+electronico.MostrarInformacion();
+
+Pedido pedido = new Pedido(1);
+pedido.AgregarItem(electronico, 1);
+pedido.AgregarItem(electronico, 5);
+
 electronico.MostrarInformacion();
 
 Libro libro = new Libro();
@@ -41,4 +23,10 @@ libro.Stock = 1500;
 libro.ISBN = "8536492365322";
 libro.Autor = "Antoine de Saint-Exupéry";
 libro.NumeroPaginas = 170;
+libro.MostrarInformacion();
+
+Pedido pedido1 = new Pedido(1);
+pedido1.AgregarItem(libro, 1);
+pedido1.AgregarItem(libro, 5);
+
 libro.MostrarInformacion();
